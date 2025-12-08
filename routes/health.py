f13d5f838274a00f6268a01114ae651a377c7dba
Victor <v.jimenezcanche@gmail.com>
2025-12-08 13:37:26 -0600

    Added Este mensaje no deberia verse to health message

--- routes/health.py
+++ routes/health.py
@@ -10,4 +10,4 @@ def health():
         "status": "ok",
         "service": "Backend Banco de Tiempo",
         "port": os.getenv("PORT", 8080),
-        "message": "El servidor está activo y funcionando correctamente."}), 200
+        "message": "El servidor está activo y funcionando correctamente. Este mensaje no deberia de verse"}), 200
